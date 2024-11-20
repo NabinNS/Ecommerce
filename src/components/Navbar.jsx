a46904f8727c00cb5f798f82a22d6ae6a38386ed
NabinNS <nabinshrestha348@gmail.com>
2024-11-20 11:02:50 +0545

    major changes in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,50 @@
-import { FaUserCircle } from "react-icons/fa"; // for profile icon
+// src/Navbar.js
+import { FaUserCircle } from "react-icons/fa";
+import { useState } from "react";
 import "./css/Navbar.css";
 import navbarlogo from "../assets/logo/navbarlogo.png";
+
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="navbar">
       <div className="navbar-left">
-        <img src={navbarlogo} alt="logo" />
+        <img src={navbarlogo} alt="logo" className="navbar-logo" />
+        <h4 className="navbar-title">
+          Battery And <br />
+          Parts Shop
+        </h4>
       </div>
-      <div className="navbar-center">
-        <input type="text" placeholder="Search..." />
+      <div className={`navbar-center ${isMenuOpen ? "active" : ""}`}>
+        <input type="text" placeholder="Search for products..." />
       </div>
       <div className="navbar-right">
         <a href="#" className="login-icon">
-          <FaUserCircle size={24} /> {/* Profile icon */}
+          <FaUserCircle size={28} />
         </a>
+      </div>
+      {/* Burger Button for Mobile */}
+      <div className="burger" onClick={toggleMenu}>
+        <div className={`line ${isMenuOpen ? "active" : ""}`}></div>
+        <div className={`line ${isMenuOpen ? "active" : ""}`}></div>
+        <div className={`line ${isMenuOpen ? "active" : ""}`}></div>
+      </div>
+
+      {/* Mobile Menu */}
+      <div className={`mobile-menu ${isMenuOpen ? "active" : ""}`}>
+        <ul>
+          <li>
+            <a href="#">Login</a>
+          </li>
+        </ul>
       </div>
     </div>
   );
 }
+
 export default Navbar;
